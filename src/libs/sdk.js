7b62bd244f17f00f1b16562ec374dfe6edbbfd04
ug.rp <ugrp@duck.com>
2024-02-20 22:59:27 +0100

    add heatmap (all jobs, and company jobs)

--- src/libs/sdk.js
+++ src/libs/sdk.js
@@ -85,6 +85,85 @@ export class JoblistSDK {
 			[limit],
 		);
 	}
+
+	async getCompanyHeatmap(slug, days = 365) {
+		const sql = `
+WITH RECURSIVE date_range AS (
+  SELECT MIN(published_date) AS min_date, MAX(published_date) AS max_date FROM jobs
+  WHERE published_date > DATE('now', '-' || ? || ' ' || 'days')
+  UNION ALL
+  SELECT date(min_date, '+1 day'), max_date FROM date_range WHERE min_date < max_date
+)
+SELECT
+  COALESCE(company_slug, ?) AS company_slug,
+  date_range.min_date AS date,
+  COALESCE(COUNT(DISTINCT ObjectId), 0) AS total,
+  strftime('%Y', date_range.min_date) AS year,
+  strftime('%u', date_range.min_date) AS dow,
+  strftime('%W', date_range.min_date) AS woy
+FROM
+  date_range
+LEFT JOIN
+  jobs ON date_range.min_date = jobs.published_date
+AND company_slug = ?
+OR company_slug is null
+GROUP BY 1,2
+ORDER BY published_date ASC;
+		`;
+		const params = [days, slug, slug];
+		const result = await this.executeQuery(sql, params);
+		if (result) {
+			const { values, columns } = result[0];
+			return values.map((row) => {
+				return row.reduce((acc, col, i) => {
+					acc[columns[i]] = col;
+					return acc;
+				}, {});
+			});
+		}
+	}
+	async getJobsHeatmap(days = 365) {
+		const sql = `
+WITH RECURSIVE date_range AS (
+  SELECT
+    MIN(published_date) AS min_date,
+    MAX(published_date) AS max_date
+  FROM jobs
+  WHERE published_date > DATE('now', '-' || ? || ' days')
+  UNION ALL
+  SELECT DATE(min_date, '+1 day'), max_date
+  FROM date_range
+  WHERE min_date < max_date
+)
+SELECT
+  date_range.min_date AS date,
+  COALESCE(COUNT(DISTINCT jobs.ObjectId), 0) AS total,
+  strftime('%Y', date_range.min_date) AS year,
+  strftime('%m', date_range.min_date) AS month,
+  strftime('%u', date_range.min_date) AS dow,
+  strftime('%j', date_range.min_date) AS doy,
+  strftime('%W', date_range.min_date) AS woy
+FROM
+  date_range
+LEFT JOIN
+  jobs ON DATE(date_range.min_date) = DATE(jobs.published_date)
+GROUP BY
+  date_range.min_date
+ORDER BY
+  date_range.min_date ASC;
+		`;
+		const params = [days];
+		const result = await this.executeQuery(sql, params);
+		if (result) {
+			const { values, columns } = result[0];
+			return values.map((row) => {
+				return row.reduce((acc, col, i) => {
+					acc[columns[i]] = col;
+					return acc;
+				}, {});
+			});
+		}
+	}
 }
 
 export default new JoblistSDK();
