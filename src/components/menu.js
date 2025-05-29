7f23679a42b3fc4be6de9376b0cdfc8f4ea6df57
4www <4www@duck.com>
2025-05-29 12:37:59 +0200

    update menu to https://dashboards.joblist.today/

--- src/components/menu.js
+++ src/components/menu.js
@@ -38,7 +38,7 @@ const MENUS = [
 	],
 	[
 		{ href: "https://api.joblist.today", textContent: "api" },
-		{ href: "https://joblist.gitlab.io/dashboards", textContent: "dashboards" },
+		{ href: "https://dashboards.joblist.today/", textContent: "dashboards" },
 		{
 			href: "https://sqlime.org/#https://workers.joblist.today/joblist.db",
 			textContent: "db.sqlite",
