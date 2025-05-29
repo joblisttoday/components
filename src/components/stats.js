e53a19d9d4918f13ed51eff093673b3cfdcabaef
4www <4www@duck.com>
2025-05-29 13:04:20 +0200

    rm logs

--- src/components/stats.js
+++ src/components/stats.js
@@ -2,27 +2,16 @@ import apiSdk from "../libs/sdk-api.js";
 import { JoblistSqlHttpvfsSDK } from "../libs/sdk-sql-httpvfs.js";
 
 export default class JoblistStats extends HTMLElement {
-	constructor() {
-		super();
-		this.sdk = apiSdk;
-	}
 	async connectedCallback() {
 		this.sdk = new JoblistSqlHttpvfsSDK(this.databaseUrl);
 		await this.sdk.initialize();
-
-		console.log("sdk", this.sdk);
-		const stats = await this.sdk.getStats();
-		console.log("stats", stats);
-		this.render(stats);
+		try {
+			const stats = await this.sdk.getStats();
+			this.render(stats);
+		} catch (error) {
+			this.textContent = "Error loading stats";
+		}
 	}
-	/* async connectedCallback() {
-		 try {
-		 const stats = await this.sdk.getStats();
-		 this.render(stats);
-		 } catch (error) {
-		 this.textContent = "Error loading stats";
-		 }
-		 } */
 
 	render(stats) {
 		const companiesText = document.createElement("p");
