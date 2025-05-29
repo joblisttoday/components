371d0e3e43073df1643f468dc4f59c38ec009ea3
4www <4www@duck.com>
2025-05-29 13:01:06 +0200

    create sdk-sql-httpvfs and use on joblist-stats

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,19 +1,28 @@
 import apiSdk from "../libs/sdk-api.js";
+import { JoblistSqlHttpvfsSDK } from "../libs/sdk-sql-httpvfs.js";
 
 export default class JoblistStats extends HTMLElement {
 	constructor() {
 		super();
 		this.sdk = apiSdk;
 	}
-
 	async connectedCallback() {
-		try {
-			const stats = await this.sdk.getStats();
-			this.render(stats);
-		} catch (error) {
-			this.textContent = "Error loading stats";
-		}
+		this.sdk = new JoblistSqlHttpvfsSDK(this.databaseUrl);
+		await this.sdk.initialize();
+
+		console.log("sdk", this.sdk);
+		const stats = await this.sdk.getStats();
+		console.log("stats", stats);
+		this.render(stats);
 	}
+	/* async connectedCallback() {
+		 try {
+		 const stats = await this.sdk.getStats();
+		 this.render(stats);
+		 } catch (error) {
+		 this.textContent = "Error loading stats";
+		 }
+		 } */
 
 	render(stats) {
 		const companiesText = document.createElement("p");
---
+++ src/libs/sdk-sql-httpvfs.js
@@ -0,0 +1,55 @@
+import { sqliteToJson } from "../utils/sqlite.js";
+import { Company } from "../utils/models.js";
+import { createDbWorker } from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+	"sql.js-httpvfs/dist/sqlite.worker.js",
+	import.meta.url,
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+const config = {
+	from: "inline",
+	config: {
+		serverMode: "full",
+		requestChunkSize: 4096,
+		url: "https://workers.joblist.today/joblist.db",
+	},
+};
+
+export class JoblistSqlHttpvfsSDK {
+	constructor(url = "https://workers.joblist.today/joblist.db") {
+		this.url = url;
+	}
+
+	async initialize() {
+		const worker = await createDbWorker(
+			[config],
+			workerUrl.toString(),
+			wasmUrl.toString(),
+			10 * 1024 * 1024,
+		);
+		this.db = worker.db;
+	}
+
+	async executeQuery(exec = "", params = []) {
+		const result = await this.db.exec(exec, [...params]);
+		return sqliteToJson(result);
+	}
+
+	async getStats() {
+		let res;
+		try {
+			res = await this.executeQuery(`
+		SELECT
+			(SELECT total_companies FROM companies_analyze) AS total_companies,
+			(SELECT total_jobs FROM jobs_analyze) AS total_jobs
+			`);
+		} catch (e) {
+			throw e;
+		}
+		return res[0];
+	}
+}
+
+export default new JoblistSqlHttpvfsSDK();
