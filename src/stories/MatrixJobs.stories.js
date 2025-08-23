f739256a5da1ab9c76525793211411c705b59467
4www <4www@duck.com>
2025-08-23 08:10:46 +0200

    fix MatrixJobs stories

--- src/stories/MatrixJobs.stories.js
+++ src/stories/MatrixJobs.stories.js
@@ -1,29 +1,5 @@
 import { html } from 'lit-html';
 import '../components/matrix-jobs.js';
-import mwc from '../libs/mwc.js';
-
-// Mock the mwc library by directly setting componentDefinitions
-if (!mwc.componentDefinitions['matrix-room']) {
-  mwc.componentDefinitions['matrix-room'] = class MatrixRoom extends HTMLElement {
-    connectedCallback() {
-      let content = 'Mock Matrix Room';
-      if (this.hasAttribute('filter')) {
-        content += ` Filter: ${this.getAttribute('filter')}`;
-      }
-      if (this.hasAttribute('show-event-info')) {
-        content += ' Show Event Info';
-      }
-      if (this.hasAttribute('show-context')) {
-        content += ' Show Context';
-      }
-      if (this.hasAttribute('origin')) {
-        content += ` Origin: ${this.getAttribute('origin')}`;
-      }
-      
-      this.textContent = content;
-    }
-  };
-}
 
 export default {
   title: 'Matrix/MatrixJobs',
@@ -39,6 +15,7 @@ export default {
 
 const Template = ({ filter, showEventInfo, showContext, origin, profileId }) => html`
   <joblist-matrix-jobs
+    mock="true"
     .filter=${filter}
     ?show-event-info=${showEventInfo}
     ?show-context=${showContext}
@@ -49,9 +26,9 @@ const Template = ({ filter, showEventInfo, showContext, origin, profileId }) => 
 
 export const Default = Template.bind({});
 Default.args = {
-  filter: { a: 1 },
-  showEventInfo: false,
-  showContext: false,
-  origin: '',
-  profileId: '',
+  filter: { types: ['today.joblist.job'] },
+  showEventInfo: true,
+  showContext: true,
+  origin: 'https://boards.joblist.today',
+  profileId: '#general.boards.joblist.today:matrix.org',
 };
